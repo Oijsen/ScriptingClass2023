a49098942f64d7e410c6eee55bc98cb6dbfa8c30
Kaitlyn Hughes <kaitycheshire@gmail.com>
2023-09-06 14:15:15 -0600

    Lab1aCode

---
+++ Unit_01a - Wrk/Lab01a.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		string myName = "Kaitlyn" ;
+		Console.WriteLine(myName);
+		int y = 10;
+		int x = 11;
+		int w = (x + y);
+		Console.WriteLine(w);
+	}
+}
